08c6181d29cf232f1bb7774733aa41cfdb7fc502
krishgarg11 <krishgarg045@gmail.com>
2026-07-29 10:21:40 +0530

    first

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
-function add (num1,num2){
-    console.log(num1,num2);
-}
-add(2,1);  //arrow function
-const addi=()=>{
-    console.log("Arrow function")
-}
-addi();
-const addi=(num1,num2)=>{
-    return num1+num2;
 
+//function in js:block code
+//syntax
+//function fname(){
+//}
+//fname();
+function add(num1, num2) {
+    console.log("Standard function result:", num1 + num2);
+    return num1 + num2;
 }
-console.log(add(2,1));
+add(10, 20);
+
+//arrow function
+//variable in js:conatiner to store data
+//var, let, const
+// syntax:()=>{}
+const addArrow = () => {
+    console.log("arrow function");
+}
+addArrow();
+
+const addArrowWithParams = (num1, num2) => {
+    return num1 + num2;
+}
+console.log("Arrow function with params result:", addArrowWithParams(10, 20));
+
+//argument:array list object 
+function addNum(...args) {
+    console.log("Arguments passed:", args);
+}
+addNum(10, 20, 30, 40, 50);
+
+//node js:runtime environment to run js code outside the browser
